a87b53af54f4758bba53d8d5aa626953a40e2c71
ruishishi <r.shishido@env-value.co.jp>
2026-05-28 14:40:25 +0900

    feat(f4): mount LibraryShell at /decks

---
+++ app/(app)/decks/page.tsx
@@ -0,0 +1,9 @@
+import { LibraryShell } from "@/app-shell/LibraryShell";
+
+export const metadata = {
+  title: "デッキ - English Audio PWA",
+};
+
+export default function DecksPage() {
+  return <LibraryShell />;
+}
