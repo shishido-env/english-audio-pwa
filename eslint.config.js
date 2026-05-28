771cae2c8cd3e9afda63cf3156cf0c6e49bb68fd
ruishishi <r.shishido@env-value.co.jp>
2026-05-28 12:45:12 +0900

    chore(f1): switch eslint to next.js configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,24 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import nextPlugin from '@next/eslint-plugin-next'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['.next', 'dist', 'node_modules']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
     ],
+    plugins: { '@next/next': nextPlugin },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+    },
     languageOptions: {
       globals: globals.browser,
     },
